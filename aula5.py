8bbcb0a18784d6be9af49e0f70426c41e7dbeb66
Gustavo Campos <115758013+gugacmp@users.noreply.github.com>
2023-08-30 21:58:54 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,70 @@
+import time
+
+class Gerenciamento():
+    def atualizar():
+        print('Atualizar')
+    def analise():
+        print('Analise')
+    def enviar():
+        print('Enviar')
+    def aprovado():
+        print('Aprovado')
+    def atualizando():
+        print('Atualizando')
+    def cancelado():
+        print('Cancelado')
+ 
+
+codigo = int(input('Informe o código :'))
+time.sleep(1)
+print('|------------API COMERCE-------------|')
+time.sleep(0.5)
+
+def api():
+    if codigo > 1 and codigo < 100:
+        print('|--------------APROVADO--------------|')
+        print(f' Pedido : {codigo} '), Gerenciamento.aprovado()
+
+    elif codigo > 100 and codigo < 500:
+        print('|--------------ANALISE---------------|')
+        print(f'Pedido : {codigo}')
+        Gerenciamento.analise()
+
+    elif codigo > 500 and codigo < 700:
+        print('|--------------CANCELADO-------------|')
+        print(f'Pedido : {codigo}')
+        Gerenciamento.cancelado()
+api()
+
+print("|====================================|")
+print("|====================================|")
+time.sleep(0.5)
+def hub():
+    print('|--------------HUB COMERCE-----------|')
+
+    def importar():
+        if codigo > 1 and codigo < 100:
+            print('|--------------IMPORTAR--------------|')
+            Gerenciamento.atualizando()
+    importar()    
+
+    def atualizar():
+        if codigo > 100 and codigo < 500:
+            print('|--------------ATUALIZAR-------------|')
+
+            Gerenciamento.atualizar()
+    atualizar()
+
+    if codigo > 500 and codigo < 700:
+        print('|--------------CANCELAR--------------|')
+
+        Gerenciamento.cancelado()
+
+hub()
+
+def venda():
+    pass
+
+
+
+        
